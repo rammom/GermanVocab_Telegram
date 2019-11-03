3a4d5588540b317d5f40ef0d295931734261e573
moose <mr.rammo@gmail.com>
2019-11-03 14:56:35 -0500

    version1.0

--- bot.js
+++ bot.js
@@ -1,18 +1,44 @@
 const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.GermanVocab_Token);
 
-bot.start(ctx => 
-	ctx.reply(`
-		Thank you for subscribing to our german vocabulary practice!
-		I hope this bot will help new learners improve their german vocabulary,
-		if you have any issues with the bot or ideas for improvements please let me know, you can 
-		reach out by messaging @mrammo.
-		
-		- Moose
-	`)
-)
+// Gather vocabulary
+const csv = require("csvtojson");
+let vocab = [];
+let choice = 0;
 
-bot.hears('hey', ctx => ctx.reply('Hello!'))
+async function setup() {
+	return new Promise(async (resolve, reject) => {
+		for (let i = 1; i < 5; ++i){
+			vocab = vocab.concat( await csv().fromFile(`./vocabulary/chapter${i}.csv`) )
+		}
+		return resolve();
+	});
+}
 
+let ask = (ctx, prepend="") => {
+	choice = Math.floor(Math.random() * Math.floor(vocab.length));
+	ctx.reply(prepend + "\n\n" + vocab[choice].en);
+}
 
-bot.launch()
+let main = async () => {
+
+	await setup();
+
+	bot.start(ctx => { 
+		ctx.reply("Thank you for subscribing to our german vocabulary practice! I hope this bot will help new learners improve their german vocabulary, if you have any issues with the bot or ideas for improvements please let me know, you can reach out by messaging @mrammo.\n\n- Moose\nType /practice to start.")
+		ask(ctx);
+	});
+
+	bot.hears(/.*/, (ctx) => {
+		let recap = "=====\n";
+		if (ctx.message.text == vocab[choice].de) recap += "✅";
+		else recap += "❌";
+		recap += `\n${vocab[choice].de}\n${vocab[choice].en}`;
+		recap += "\n====="
+		ask(ctx, recap);
+	});
+
+	bot.launch()
+}
+
+main();
